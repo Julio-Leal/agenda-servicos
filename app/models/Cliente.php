99cdf08cfd987a3878c500479ea4e1b05e293d0f
Julio-Leal <cavalcantejulio522@gmail.com>
2026-09-15 00:15:28 -0300

    Update: Corrige a classe Cliente.php que antes era uma copia da classe Usuario.php

--- app/models/Cliente.php
+++ app/models/Cliente.php
@@ -1,62 +1,87 @@
-<?php 
-    class Usuario {
-        private int $id;
-        private String $nome;
-        private String $email;
-        private String $senha;
-        private String $tipo;
-        private String $criadoEm;
+<?php
 
-        public function __construct( 
-            string $nome, 
-            string $email, 
-            string $senha, 
-            string $tipo = 'FUNCIONARIO' 
-        ) { 
-            $this->nome = $nome; 
-            $this->email = $email; 
-            $this->senha = $senha; 
-            $this->tipo = $tipo; 
-        }
+namespace App\Models;
 
-        public function getId(): ?int {
-            return $this->id ?? null;
-        }
+class Cliente {
+    private int $id;
+    private string $nome;
+    private string $cpf;
+    private string $telefone;
+    private ?string $email;
+    private ?string $dataNascimento;
+    private string $criadoEm;
 
-        public function getNome(): String {
-            return $this->nome;
-        }
-
-        public function setNome(String $nome): void {
-            $this->nome = $nome;
-        } 
-
-        public function getEmail(): String {
-            return $this->email;
-        }
-
-        public function setEmail(String $email): void {
-            $this->email = $email;
-        }
-
-        public function getSenha(): String {
-            return $this->senha;
-        }
-
-        public function setSenha(String $senha): void {
-            $this->senha = $senha;
-        }
-
-        public function getTipo(): String {
-            return $this->tipo;
-        }
-
-        public function setTipo(String $tipo): void {
-            $this->tipo = $tipo;
-        }
-
-        public function getCriadoEm(): ?String {
-            return $this->criadoEm ?? null;
-        }
+    public function __construct(
+        string $nome,
+        string $cpf,
+        string $telefone,
+        ?string $email = null,
+        ?string $dataNascimento = null
+    ) {
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+        $this->telefone = $telefone;
+        $this->email = $email;
+        $this->dataNascimento = $dataNascimento;
     }
-?>
+
+    public function getId(): ?int
+    {
+        return $this->id ?? null;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): void
+    {
+        $this->cpf = $cpf;
+    }
+
+    public function getTelefone(): string
+    {
+        return $this->telefone;
+    }
+
+    public function setTelefone(string $telefone): void
+    {
+        $this->telefone = $telefone;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getDataNascimento(): ?string
+    {
+        return $this->dataNascimento;
+    }
+
+    public function setDataNascimento(?string $dataNascimento): void
+    {
+        $this->dataNascimento = $dataNascimento;
+    }
+
+    public function getCriadoEm(): ?string
+    {
+        return $this->criadoEm ?? null;
+    }
+}
