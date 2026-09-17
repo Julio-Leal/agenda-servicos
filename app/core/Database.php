9d67edf1a33049e9bf89eea9e4ec8f66e8a0c7ac
Julio-Leal <cavalcantejulio522@gmail.com>
2026-09-17 20:55:13 -0300

    Update: Implementa conexao com banco de dados na classe Database.php

--- app/core/Database.php
+++ app/core/Database.php
@@ -2,6 +2,30 @@
     namespace App\Core;
 
     class Database {
-        
+        private $connection;
+
+        public function getConnection() {
+            $config = require __DIR__ . '/../../config/database.php';
+
+            $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['database']};charset=utf8mb4";
+
+            $this->connection = new \PDO(
+                $dsn,
+                $config['username'],
+                $config['password']
+            );
+
+            $this->connection->setAttribute(
+                \PDO::ATTR_ERRMODE,
+                \PDO::ERRMODE_EXCEPTION
+            );
+
+            $this->connection->setAttribute(
+                \PDO::ATTR_DEFAULT_FETCH_MODE,
+                \PDO::FETCH_ASSOC
+            );
+
+            return $this->connection;
+        }
     }
 ?>
