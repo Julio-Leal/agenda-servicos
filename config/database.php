eed35cab610b6e36b4120000161f3769b82ab117
Julio-Leal <cavalcantejulio522@gmail.com>
2026-09-17 20:56:02 -0300

    Create: Cria o arquivo database.php para conter a configuracao default de acesso ao bd

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+    return [
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => 'agenda_servicos',
+        'username' => 'root',
+        'password' => ''
+    ];
+?>
