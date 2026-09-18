464bb805bf1a6af6800c72192a1a201acb5e7a5a
Julio-Leal <cavalcantejulio522@gmail.com>
2026-09-17 21:15:44 -0300

    Create: Cria a classe ClienteRepository com a conexao com o bd e o metodo create

---
+++ app/Repositories/ClienteRepository.php
@@ -0,0 +1,31 @@
+<?php 
+    namespace App\Repositories;
+
+    use App\Core\Database;
+    use App\Models\CLiente;
+
+    class ClienteRepository {
+        private $connection;
+
+        public function __construct() {
+            $database = new Database();
+            $this->connection = $database->getConnection();
+        }
+
+        public function create(Cliente $cliente) {
+            $sql = "INSERT INTO cliente
+                    (NOME, CPF, TELEFONE, EMAIL, DATA_NASCIMENTO)
+                    VALUES (:nome, :cpf, :telefone, :email, :dataNascimento)";
+
+            $stmt = $this->connection->prepare($sql);
+
+            $stmt->bindValue(':nome', $cliente->getNome());
+            $stmt->bindValue(':cpf', $cliente->getCpf());
+            $stmt->bindValue(':telefone', $cliente->getTelefone());
+            $stmt->bindValue(':email', $cliente->getEmail());
+            $stmt->bindValue(':dataNascimento', $cliente->getDataNascimento());
+
+            return $stmt->execute();
+        }
+    }
+?>
