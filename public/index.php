ef80228b43f211314a3451aae78f3157ad604571
Julio-Leal <cavalcantejulio522@gmail.com>
2026-09-22 22:30:31 -0300

    Update: Simplifica o arquivo /public/index.php para ativar o uso do Router criado

--- public/index.php
+++ public/index.php
@@ -2,21 +2,11 @@
 
     require_once __DIR__ . '/../vendor/autoload.php';
 
+    use App\Core\Router;
+
     $pagina = $_GET['pagina'] ?? null;
 
-    if ($pagina === 'clientes') {
+    $router = new Router();
 
-        require __DIR__ . '/../app/Views/clientes/index.php';
-
-        exit;
-    }
-
-    if ($pagina === 'dashboard') {
-
-        require __DIR__ . '/../app/Views/dashboard/index.php';
-
-        exit;
-    }
-
-    require __DIR__ . '/../app/Views/layouts/dashboard.php';
+    $router->handle($pagina);
 ?>
