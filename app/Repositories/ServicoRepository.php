bc79ae271e73ab907dc34cea1f100bf9c35c195b
Julio-Leal <cavalcantejulio522@gmail.com>
2026-09-23 23:44:31 -0300

    Update: Corrige nome da classe Database() e adiciona o metodo update

--- app/Repositories/ServicoRepository.php
+++ app/Repositories/ServicoRepository.php
@@ -2,13 +2,13 @@
     namespace App\Repositories;
 
     use App\Core\Database;
-    use App\MOdels\Servico;
+    use App\Models\Servico;
 
     class ServicoRepository {
         private $connection;
 
         public function __construct() {
-            $database = new DataBase();
+            $database = new Database();
             $this->connection = $database->getConnection();
         }
 
@@ -40,13 +40,35 @@
 
         public function findById(int $id) {
             $sql = "SELECT *
-                    FROM cliente
+                    FROM servico
                     WHERE ID = :id";
             
             $stmt = $this->connection->prepare($sql);
             $stmt->bindValue(':id', $id, \PDO::PARAM_INT);
             $stmt->execute();
             return $stmt->fetch();
+        }
+
+        public function update(Servico $servico) {
+            $sql = "UPDATE servico
+                    SET 
+                        NOME = :nome, 
+                        DESCRICAO = :descricao, 
+                        DURACAO = :duracao, 
+                        PRECO = :preco, 
+                        ATIVO = :ativo
+                    WHERE ID = :id";
+
+            $stmt = $this->connection->prepare($sql);
+
+            $stmt->bindValue(':nome', $servico->getNome());
+            $stmt->bindValue(':descricao', $servico->getDescricao());
+            $stmt->bindValue(':duracao', $servico->getDuracao());
+            $stmt->bindValue(':preco', $servico->getPreco());
+            $stmt->bindValue(':ativo', $servico->isAtivo());
+            $stmt->bindValue(':id', $servico->getId(), \PDO::PARAM_INT);
+
+            return $stmt->execute();
         }
     }
 ?>
