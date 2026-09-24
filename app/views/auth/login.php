87ab400fb972354f0e9da740955da8295303ff61
Júlio Leal <91677208+Julio-Leal@users.noreply.github.com>
2026-09-24 15:51:20 +0000

    Update: Testa caminho que aponta para o css

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Agenda de Serviços — Login</title>
+    <link rel="stylesheet" href="/assets/css/login.css">
 
-    <link rel="stylesheet" href="/public/assets/css/login.css">
 </head>
 
 <body>
